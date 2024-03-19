e3e52f60e6a5eea11a7516445a6a4db287cfe6b9
GolAliSer <114601456+GolAliSer@users.noreply.github.com>
2024-03-19 17:54:16 +0300

    Update 1418.cpp

--- 1418.cpp
+++ 1418.cpp
@@ -1,18 +1,18 @@
 ﻿#include <iostream>
-#include <set>
+#include <set> 
 
 int main() {
-    int N;
-    std::cin >> N;
+    int N; //переменная для количества чисел
+    std::cin >> N; //вводим количество чисел
 
-    std::set<int> numbers;
-    for (int i = 0; i < N; ++i) {
+    std::set<int> numbers; //создаем множество чисел
+    for (int i = 0; i < N; ++i) { //через цикл заполняем числами контейнер
         int num;
         std::cin >> num;
-        numbers.insert(num);
+        numbers.insert(num); //добавляем введенное число в контейнер
     }
 
-    int uniqueCount = numbers.size();
+    int uniqueCount = numbers.size(); //так как set хранит каждый элемент лишь в одном экземпляре, размер коллекции будет равен количеству разных элементов, так как все дубликаты не добавляются в set
     std::cout << uniqueCount << std::endl;
 
     return 0;
